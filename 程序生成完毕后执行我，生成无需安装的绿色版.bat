69811142d2063b9a3273671d79de8b0cd9c7cf1b
ILostMyPig <18015262576@189.cn>
2023-03-06 21:08:17 +0800

    V6.2.0
    V6.2.0

---
+++ 程序生成完毕后执行我，生成无需安装的绿色版.bat
@@ -0,0 +1,13 @@
+rd /s /q 霸屏浏览器
+md 霸屏浏览器
+copy release\*.exe 霸屏浏览器\
+copy release\*.dll 霸屏浏览器\
+del 霸屏浏览器\*.vshost.exe
+xcopy /e 程序所需的其它文件\* 霸屏浏览器\
+cls
+@echo off 
+explorer 霸屏浏览器\
+echo 已整理为霸屏浏览器文件夹。
+echo 霸屏浏览器启动程序 ：cs_fxb_win_hook.exe。
+echo 设置程序           ：set_ini.exe。
+pause
